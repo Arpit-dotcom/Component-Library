2f1ff2fdc50b15a098e28686408aa971683d0ae3
Arpit <arpitkumar01923@gmail.com>
2022-04-17 11:21:18 +0530

    make sidebar responsive

--- script.js
+++ script.js
@@ -4,7 +4,16 @@ const navMenu = document.querySelector(".nav-list");
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
-  console.log("hi");
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
+}
+
+const header = document.querySelector(".docs-header");
+const sidebar = document.querySelector(".drawer");
+
+header.addEventListener("click", sideMenu);
+
+function sideMenu() {
+  header.classList.toggle("active");
+  sidebar.classList.toggle("active");
 }
